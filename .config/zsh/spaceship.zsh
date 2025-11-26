03782fb9512c6ca4cdf35efade324c89f80d7c7e
José Rui Barros <joserui@joseruibarros.com>
2025-11-26 11:44:12 +0100

    spaceship config simplication

--- .config/zsh/spaceship.zsh
+++ .config/zsh/spaceship.zsh
@@ -9,35 +9,5 @@ SPACESHIP_ASYNC_SHOW_COUNT=true
 
 # Vi mode icon does not work without this
 spaceship_vi_mode_enable
+spaceship add --before line_sep vi_mode
 
-# Lmao
-# SPACESHIP_PROMPT_ASYNC=false
-SPACESHIP_GIT_ASYNC=FALSE # workaround for now
-
-SPACESHIP_PROMPT_ORDER=(
-  time           # Time stamps section
-  user           # Username section
-  dir            # Current directory section
-  host           # Hostname section
-  git            # Git section (git_branch + git_status)
-  package        # Package version
-  # node           # Node.js section
-  # python         # Python section
-  # golang         # Go section
-  # kotlin         # Kotlin section
-  # java           # Java section
-  # lua            # Lua section
-  # docker         # Docker section
-  # docker_compose # Docker section
-  # terraform      # Terraform workspace section
-  aws            # Amazon Web Services section
-  venv           # virtualenv section
-  exec_time      # Execution time
-  async          # Async jobs indicator
-  vi_mode        # Vi mode
-  line_sep       # Line break
-  jobs           # Background jobs indicator
-  exit_code      # Exit code section
-  sudo           # Sudo indicator
-  char           # Prompt character
-)
